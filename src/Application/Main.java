eba5b226eee2f5688aec5c39ca5b4d02a3b806b8
JG <ladrenrights@gmail.com>
2022-06-05 20:53:47 -0700

    Added open file function

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,6 +1,38 @@
 package Application;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 
 public class Main
 {
-
+	public static void main(String[] args)
+	{
+		Scanner filePointer = openFile(args);
+		System.out.println(filePointer.toString());
+		
+	}
+	
+	public static Scanner openFile(String[] args) 
+	{
+		if(args.length == 0) 
+		{
+			System.out.println("No file has been provided for the program");
+			System.exit(0);
+		}
+		
+		Scanner s = null;
+		try 
+		{
+			s = new Scanner(new File(args[0]));
+		}
+		catch(FileNotFoundException e) 
+		{
+			System.out.println("The entered file cannot be found");
+			System.exit(0);
+		}
+		return s;
+		
+	}
 }
