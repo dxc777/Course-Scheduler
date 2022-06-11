c356c5dbc356ceca927f0b41ef728154054cec5a
JG <ladrenrights@gmail.com>
2022-06-10 23:17:07 -0700

    Test run of course scheduler

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,18 +2,30 @@ package Application;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 
 public class Main
 {
+	static final int MAX_UNITS = 15;
 	public static void main(String[] args)
 	{
+		args = new String[]{"Test.txt"};
 		Scanner filePointer = openFile(args);
-		System.out.println(filePointer.toString());
+		Parser p = new Parser(filePointer);
 		
+		ArrayList<Course> courses = p.getCourseList();
+		CourseScheduler cs = new CourseScheduler(p.getGraph(),p.getCourseList(),MAX_UNITS);
+		System.out.println(cs.currentState());
+		cs.pick(0);
+		System.out.println(cs.currentState());
+		cs.pick(0);
+		System.out.println(cs.currentState());
 	}
 	
+
 	public static Scanner openFile(String[] args) 
 	{
 		if(args.length == 0) 
