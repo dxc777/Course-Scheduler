6eaf1bf622ba7cacf1166dcce92256778df95573
JG <ladrenrights@gmail.com>
2022-06-16 16:28:27 -0700

    Added comment

--- src/Application/Course.java
+++ src/Application/Course.java
@@ -1,5 +1,11 @@
 package Application;
 
+/**
+ * I used a record here and that will generate getters and setters as well as 
+ * make the class immutable
+ * @author J
+ *
+ */
 public record Course(String courseName, int units)
 {
 	public Course
