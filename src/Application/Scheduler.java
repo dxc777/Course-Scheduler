49b6c147f1572668623768306ff17dad5df382a6
JG <ladrenrights@gmail.com>
2022-07-14 19:44:19 -0700

    Added new state to be returned

--- src/Application/Scheduler.java
+++ src/Application/Scheduler.java
@@ -155,6 +155,10 @@ public class Scheduler
 		{
 			return State.UNIT_MAX;
 		}
+		else if(takeableClasses.isEmpty()) 
+		{
+			return State.NO_CLASSES_LEFT;
+		}
 		else 
 		{
 			return State.UNIT_NOT_FULL;
